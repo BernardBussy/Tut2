6443182bd3b65ef9f121008fa1eb99df30f40a49
BernardBussy <213533986@stu.ukzn.ac.za>
2015-03-07 23:27:10 +0200

    Operator and Get/Set Functions Created for Fraction Class

--- Tut2/Fraction.h
+++ Tut2/Fraction.h
@@ -1,8 +1,18 @@
 #pragma once
 class Fraction
 {
+private:
+	int numerator, denominator, wholeNumber;
+	void simplify(Fraction &);
 public:
 	Fraction();
 	~Fraction();
+	void SetDenominator(int);
+	void SetNumerator(int);
+	void Print();
+	Fraction operator+(Fraction &); // + Operator Overload
+	Fraction operator-(Fraction &); // - Operator Overload
+	Fraction operator*(Fraction &); // * Operator Overload
+	Fraction operator/(Fraction &); // / Operator Overload
 };
 
