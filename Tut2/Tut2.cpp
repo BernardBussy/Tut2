711a52ee4f717559e4889af8ee97e671fa23a4d3
BernardBussy <213533986@stu.ukzn.ac.za>
2015-03-08 10:26:43 +0200

    Input Fraction Function Written

--- Tut2/Tut2.cpp
+++ Tut2/Tut2.cpp
@@ -4,28 +4,50 @@
 #include <iostream>
 #include "Fraction.h"
 using namespace std;
-
+int dog();
+Fraction InputFraction();
 
 int main()
 {
-	int temp;
 	Fraction A, B, result;
-
-	cout << "Enter Numerator for Fraction A!" << endl;
-	cin >> temp;
-	A.SetNumerator(temp);
-	cout << "Enter Denominator for Fraction A!" << endl;
-	cin >> temp;
-	A.SetDenominator(temp);
-
-	cout << "Enter Numerator for Fraction B!" << endl;
-	cin >> temp;
-	B.SetNumerator(temp);
-	cout << "Enter Denominator for Fraction B!" << endl;
-	cin >> temp;
-	B.SetDenominator(temp);
-
+	cout << "For Fraction A" << endl;
+	A = InputFraction();
+	cout << "For Fraction B" << endl;
+	B = InputFraction();
+	
+	cout << "A-B" << endl;
 	result = A - B;
+	result.Print();
+
+	cout << "A+B" << endl;
+	result = A + B;
+	result.Print();
+
+	cout << "A*B" << endl;
+	result = A*B;
+	result.Print();
+
+	cout << "A/B" << endl;
+	result = A/B;
 	result.Print();
 }
 
+Fraction InputFraction()
+{
+	int temp;
+	Fraction tempF;
+	cout << "Enter Numerator of Fraction:" << endl;
+	cin >> temp;
+	tempF.SetNumerator(temp);
+	EnterDenominator:
+	cout << "Enter Denominator of Fraction:" << endl;
+	cin >> temp;
+	if (temp == 0)
+	{
+		cout << "You cannot have a denominator as Zero!" << endl;
+		goto EnterDenominator;
+	}
+	tempF.SetDenominator(temp);
+
+	return tempF;
+}
